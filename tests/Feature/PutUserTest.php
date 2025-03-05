51fd7345dbf174b7a07157c1a1cd08288de40fcd
Ingrid Stemer <ingrid.stemer@live.fr>
2025-03-05 12:22:35 +0100

    fix : ajout du model user manquant dans PutUserTest

--- tests/Feature/PutUserTest.php
+++ tests/Feature/PutUserTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Laravel\Sanctum\Sanctum;
+use App\Models\User;
 
 class PutUserTest extends TestCase
 {
