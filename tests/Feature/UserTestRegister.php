6e3a5fec83355a405c0694bd5b773cd7608dd0f9
Ingrid Stemer <ingrid.stemer@live.fr>
2025-03-05 10:37:19 +0100

    userTestResgister 2/3 test write

--- tests/Feature/UserTestRegister.php
+++ tests/Feature/UserTestRegister.php
@@ -5,16 +5,56 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class UserTestRegister extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase; // Réinitialise la base de données entre les tests
 
-        $response->assertStatus(200);
+    public function test_1_inscription_ok(): void
+    {
+        // Créer un utilisateur en base de données
+        $user = [
+            'email' => 'newuser@example.com',
+            'password' => bcrypt('password123'), // Le mot de passe doit être hashé
+            "name" => "New User"
+        ];
+        // Envoyer une requête POST à /api/auth/register
+        $response = $this->postJson('/api/auth/register', [
+            'email' => 'newuser@example.com',
+            'password' => 'password123',
+            "name" => "New User"
+        ]);
+
+        // Vérifier le statut de réponse 201
+        $response->assertStatus(201);
+
+        // Vérifier que le token est bien retourné
+        $response->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in'
+        ]);
+    }
+
+    public function test_2_inscription_avec_email_existant(): void
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password123'),
+        ]);
+
+        // Envoyer une requête POST à /api/auth/register
+        $response = $this->postJson('/api/auth/register', [
+            'email' => 'newuser@example.com',
+            'password' => 'password123',
+            "name" => "New User2"
+        ]);
+
+        // Vérifier le statut de réponse 400
+        $response->assertStatus(400);
+
+            // Vérifier que le message d'erreur est retourné
+            $response->assertJson(['message' => 'Email déjà utilisé']);
     }
 }
